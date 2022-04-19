a589289efde6f133723a6454d1b468275759a52d
marqueewinq <marqueewinq@gmail.com>
2022-04-19 16:28:10 +0300

    added get-next-tag

---
+++ bash.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+export RELEASE_REGEX_ALPHA="^v([0-9]+).([0-9]+).([0-9]+)-alpha([0-9]+)$"
+export RELEASE_REGEX="^v([0-9]+).([0-9]+).([0-9]+)$"
+
+function get_new_tag() {
+  latest_tag=$1
+  if [[ ${latest_tag} =~ ${RELEASE_REGEX_ALPHA} ]]; then
+    export NEW_TAG=$(echo "${latest_tag}" | sed -e "s/alpha${BASH_REMATCH[4]}/alpha$((${BASH_REMATCH[4]} + 1))/g")
+  else
+    if [[ ${latest_tag} =~ ${RELEASE_REGEX} ]]; then
+      export NEW_TAG=$(echo "${latest_tag}" | sed -e "s/.${BASH_REMATCH[3]}$/.$((${BASH_REMATCH[3]} + 1))/g")
+    else
+      return 1
+    fi
+  fi
+
+  echo ${NEW_TAG}
+  return 0
+}
+
+echo $(get_new_tag "v1.0.32-alpha14")
+echo $(get_new_tag "v1.0.0-alpha17")
+echo $(get_new_tag "v1.0.1")
+echo $(get_new_tag $(get_new_tag "v1.0.1"))
